8f7babdb5ce01137a3608f485851c715c76958ac
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-27 02:20:25 +0700

    contoh komposisi kelas komputer

---
+++ src/Komposisi/TestKomputer.java
@@ -0,0 +1,9 @@
+package Komposisi;
+
+public class TestKomputer {
+    public static void main(String[] args) {
+        //Membuat Objek
+        Komputer myComputer = new Komputer("PC01", 8000000);
+        myComputer.displaySpec();
+    }
+}
