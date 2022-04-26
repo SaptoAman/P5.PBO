cac66647521230cbf69c07bca7be8ddfa122b98d
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-27 01:59:38 +0700

    inheritance

---
+++ src/Game/Pocong.java
@@ -0,0 +1,12 @@
+package Game;
+
+public class Pocong extends Enemy{
+    //Membuat Method
+    public Pocong (String name, int hp, int attackPoin) {
+        super (name, hp, attackPoin);
+    }
+
+    void jump () {
+        System.out.println("Pocong Loncat-Loncat!");
+    }
+}
